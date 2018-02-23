1650c19ace734e31590e7e671b3cc65cd5a4225b
parnapiano <paran_piano@hotmail.com>
2018-02-23 20:00:53 +0900

    update gitignore

--- SoftwareBake/SoftwareBake/urls.py
+++ SoftwareBake/SoftwareBake/urls.py
@@ -1,22 +1,7 @@
-"""
-Definition of urls for SoftwareBake.
-"""
-
-from datetime import datetime
-from django.conf.urls import url
-import django.contrib.auth.views
-
-
-# Uncomment the next lines to enable the admin:
-# from django.conf.urls import include
-# from django.contrib import admin
-# admin.autodiscover()
+from django.conf.urls import include, url
+from django.contrib import admin
 
 urlpatterns = [
-    # Examples:
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^blog/', include('blog.urls')),
+    url(r'^admin/', admin.site.urls),
 ]
