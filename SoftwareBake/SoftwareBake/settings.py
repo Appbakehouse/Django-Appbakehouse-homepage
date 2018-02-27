4eb8c0cca448fde9d9d4f6d8104499018fb87b17
parnapiano <paran_piano@hotmail.com>
2018-02-27 10:26:12 +0900

    add bookmakr url and app name to settings

--- SoftwareBake/SoftwareBake/settings.py
+++ SoftwareBake/SoftwareBake/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.apps.blogConfig'
+    'blog.apps.blogConfig',
+    'bookmark.apps.bookmarkConfig',
 ]
 
 MIDDLEWARE_CLASSES = [
--- SoftwareBake/SoftwareBake/urls.py
+++ SoftwareBake/SoftwareBake/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^blog/', include('blog.urls')),
+    
     url(r'^admin/', admin.site.urls),
+    url(r'^blog/', include('blog.urls')),
+    url(r'^bookmark/', include('bookmark.urls')),
 ]
