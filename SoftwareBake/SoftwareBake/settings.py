1bd595e1d420d6866221677863fe9e8a112b428f
parnapiano <paran_piano@hotmail.com>
2018-02-28 11:43:19 +0900

    add python package for tagging, imanage handling , comment add tagging app to settings add field in POST class migrate the change to DB

--- SoftwareBake/SoftwareBake/settings.py
+++ SoftwareBake/SoftwareBake/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.blogConfig',
     'bookmark.apps.bookmarkConfig',
+    'tagging.apps.TaggingConfig',
 ]
 
 MIDDLEWARE_CLASSES = [
