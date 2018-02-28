9831e5a614ba848d12f5deb915ce556710e0e164
parnapiano <paran_piano@hotmail.com>
2018-02-28 10:19:23 +0900

    add rul and view for home page

--- SoftwareBake/SoftwareBake/urls.py
+++ SoftwareBake/SoftwareBake/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from SoftwareBake.view import HomeView
+
 urlpatterns = [
     
     url(r'^admin/', admin.site.urls),
+
+    url(r'^$', HomeView.as_view(), name='home'),
     url(r'^blog/', include('blog.urls')),
     url(r'^bookmark/', include('bookmark.urls')),
 ]
