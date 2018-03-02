2936a73d853f04b3e929b37d4b896ad9d3021e01
parnapiano <paran_piano@hotmail.com>
2018-03-02 10:53:13 +0900

    add LoginRequiredMixin class to be used blog, bookmark app

--- SoftwareBake/SoftwareBake/view.py
+++ SoftwareBake/SoftwareBake/view.py
@@ -4,6 +4,8 @@ from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.core.urlresolvers import reverse_lazy
 
+from django.contrib.auth.decorators import login_required
+
 #--- TemplateView
 class HomeView(TemplateView):
     template_name = 'home.html'
@@ -14,5 +16,12 @@ class UserCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('register_done')
 
+
 class UserCreateDoneTV(TemplateView):
     template_name = 'registration/register_done.html'
+
+class LoginRequiredMixin(object):
+    @classmethod
+    def as_view(cls, **initkwargs):
+        view = super(LoginRequiredMixin, cls).as_view(**initkwargs)
+        return login_required(view)
