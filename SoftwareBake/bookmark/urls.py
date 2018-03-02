0b4847a9aca82cdb5d39ca0ef7aaf348cc562d59
parnapiano <paran_piano@hotmail.com>
2018-03-02 10:56:07 +0900

    add urls for model's CRUD in blog, bookmark apps

--- SoftwareBake/bookmark/urls.py
+++ SoftwareBake/bookmark/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 
 from . import views
-from bookmark.views import IndexView , DetailView
+from bookmark.views import *
 
 app_name = 'bookmark'
 urlpatterns = [
@@ -9,4 +9,24 @@ urlpatterns = [
     # Class-based views for Bookmark app
     url(r'^$', IndexView.as_view(), name='index'),
     url(r'^(?P<pk>\d+)/$', DetailView.as_view(), name='detail'),
+
+    # Example: /add/
+    url(r'^add/$',
+        BookmarkCreateView.as_view(), name="add",
+    ),
+
+    # Example: /change/
+    url(r'^change/$',
+        BookmarkChangeLV.as_view(), name="change",
+    ),
+
+    # Example: /99/update/
+    url(r'^(?P<pk>[0-9]+)/update/$',
+        BookmarkUpdateView.as_view(), name="update",
+    ),
+
+    # Example: /99/delete/
+    url(r'^(?P<pk>[0-9]+)/delete/$',
+        BookmarkDeleteView.as_view(), name="delete",
+    ),
 ]
